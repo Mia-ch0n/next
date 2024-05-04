75ccf5b000bccc8237529bb66a2c0d7c9c6391b1
Meryam Rakkez <rakkezmeryam@gmail.com>
2024-05-04 15:27:42 +0100

    admin logout and middleware

--- app/AdminDash/page.jsx
+++ app/AdminDash/page.jsx
@@ -1,7 +1,10 @@
 'use client'
 import Link from 'next/link';
+import { signOut } from 'next-auth/react';
 import { Fragment, useState } from 'react'
 import { Dialog, Transition } from '@headlessui/react'
+import { Menu } from '@headlessui/react'
+import { ChevronDownIcon } from '@heroicons/react/20/solid'
 import {
   ChartBarSquareIcon,
   Cog6ToothIcon,
@@ -13,7 +16,7 @@ import {
 import { Bars3Icon, MagnifyingGlassIcon } from '@heroicons/react/20/solid'
 
 const navigation = [
-    { name: 'User s Activity', href: '/AdminDash', icon: SignalIcon, current: false },
+  { name: 'User s Activity', href: '/AdminDash', icon: SignalIcon, current: false },
   { name: 'Create Challenge', href: '/CreateChallenge', icon: PuzzlePieceIcon, current: false },
   { name: 'Settings', href: '#', icon: Cog6ToothIcon, current: false },
 ]
@@ -30,10 +33,14 @@ const secondaryNavigation = [
 ]
 const stats = [
   { name: 'Number of active users', value: '20' },
-  { name: 'Total number of question posted', value: '20',  },
+  { name: 'Total number of question posted', value: '20', },
   { name: 'Total number of question answered', value: '10' },
   { name: 'most active team', value: 'web' },
-  
+
+]
+const userNavigation = [
+  { name: 'Your profile', href: '/UserProfile' },
+  { name: 'Sign out', onClick: signOut },
 ]
 const statuses = { Completed: 'text-green-400 bg-green-400/10', Error: 'text-rose-400 bg-rose-400/10' }
 const activityItems = [
@@ -50,7 +57,7 @@ const activityItems = [
     date: '45 minutes ago',
     dateTime: '2023-01-23T11:00',
   },
-  
+
 ]
 
 function classNames(...classes) {
@@ -59,10 +66,19 @@ function classNames(...classes) {
 
 export default function Example() {
   const [sidebarOpen, setSidebarOpen] = useState(false)
+  const handleSignOut = async () => {
+    const data = await signOut({ redirect: false });
+    console.log(data);
+    if (!data.error) {
+      window.location.href = '/signIn';
+    } else {
+      console.error('Error signing out:', data.error);
+    }
+  };
 
   return (
     <>
-      
+
       <div className='block w-screen '>
         <Transition.Root show={sidebarOpen} as={Fragment}>
           <Dialog as="div" className="relative z-50 xl:hidden" onClose={setSidebarOpen}>
@@ -120,7 +136,7 @@ export default function Example() {
                           <ul role="list" className="-mx-2 space-y-1">
                             {navigation.map((item) => (
                               <li key={item.name}>
-                               <Link
+                                <Link
                                   href={item.href}
                                   className={classNames(
                                     item.current
@@ -131,7 +147,7 @@ export default function Example() {
                                 >
                                   <item.icon className="h-6 w-6 shrink-0" aria-hidden="true" />
                                   {item.name}
-                               </Link>
+                                </Link>
                               </li>
                             ))}
                           </ul>
@@ -187,12 +203,12 @@ export default function Example() {
           {/* Sidebar component, swap this element with another sidebar if you like */}
           <div className="flex grow flex-col gap-y-5 overflow-y-auto bg-gray-900 px-6 ring-1 ring-grey/5">
             <div className="flex h-16 shrink-0 items-center">
-            <Link href="/" className="flex gap-2 flex-center">   
-            <img
-                className="h-8 w-auto"
-                src="/assets/images/mobelitee.png?color=indigo&shade=500"
-                alt=""
-              />
+              <Link href="/" className="flex gap-2 flex-center">
+                <img
+                  className="h-8 w-auto"
+                  src="/assets/images/mobelitee.png?color=indigo&shade=500"
+                  alt=""
+                />
               </Link>
             </div>
             <nav className="flex flex-1 flex-col">
@@ -217,7 +233,7 @@ export default function Example() {
                     ))}
                   </ul>
                 </li>
-                
+
               </ul>
             </nav>
           </div>
@@ -251,6 +267,49 @@ export default function Example() {
                 </div>
               </form>
             </div>
+            <Menu as="div" className="relative">
+              <Menu.Button className="-m-1.5 flex items-center p-1.5">
+                <span className="sr-only">Open user menu</span>
+                <img
+                  className="h-8 w-8 rounded-full bg-gray-50"
+                  src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                  alt=""
+                />
+                <span className="hidden lg:flex lg:items-center">
+                  <span className="ml-4 text-sm font-semibold leading-6 text-gray-900" aria-hidden="true">
+                    Tom Cook
+                  </span>
+                  <ChevronDownIcon className="ml-2 h-5 w-5 text-gray-400" aria-hidden="true" />
+                </span>
+              </Menu.Button>
+              <Transition
+                as={Fragment}
+                enter="transition ease-out duration-100"
+                enterFrom="transform opacity-0 scale-95"
+                enterTo="transform opacity-100 scale-100"
+                leave="transition ease-in duration-75"
+                leaveFrom="transform opacity-100 scale-100"
+                leaveTo="transform opacity-0 scale-95"
+              >
+                <Menu.Items className="absolute right-0 z-10 mt-2.5 w-32 origin-top-right rounded-md bg-white py-2 shadow-lg ring-1 ring-gray-900/5 focus:outline-none">
+                  {userNavigation.map((item) => (
+                    <Menu.Item key={item.name}>
+                      {({ active }) => (
+                        <button
+                          onClick={handleSignOut}
+                          className={classNames(
+                            active ? 'bg-gray-50' : '',
+                            'block px-3 py-1 text-sm leading-6 text-gray-900'
+                          )}
+                        >
+                          {item.name}
+                        </button>
+                      )}
+                    </Menu.Item>
+                  ))}
+                </Menu.Items>
+              </Transition>
+            </Menu>
           </div>
 
           <div className=''>
@@ -329,12 +388,12 @@ export default function Example() {
                       Questions answered
                     </th>
                     <th scope="col" className="py-2 pl-0 pr-4 text-right font-semibold sm:pr-8 sm:text-left lg:pr-20">
-                    Posts
+                      Posts
                     </th>
                     <th scope="col" className="hidden py-2 pl-0 pr-8 font-semibold md:table-cell lg:pr-20">
                       UPs
                     </th>
-                    
+
                   </tr>
                 </thead>
                 <tbody className="divide-y divide-grey/5">
@@ -368,7 +427,7 @@ export default function Example() {
                       <td className="hidden py-4 pl-0 pr-8 text-sm leading-6 text-gray-400 md:table-cell lg:pr-20">
                         {item.ups}
                       </td>
-                      
+
                     </tr>
                   ))}
                 </tbody>
